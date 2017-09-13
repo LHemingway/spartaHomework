c5bf4190fd70a5a3bb0c58222abee62af14c7523
Luke Hemingway <lukehemingway66@gmail.com>
2017-09-13 10:39:48 +0100

    another because it's still red?

--- w02d02/js-dom-checkers/app.js
+++ w02d02/js-dom-checkers/app.js
@@ -6,16 +6,47 @@ function addListItem(value) {
     list.append(newItem);
 }
 
+//Add Squares
 for (var i = 0; i < 64; i++) {
 	addListItem("");
 }
 
-var whiteItems = document.getElementsByTagName("li");
-for (var i = 0; i < 64; i += 2) {
-	whiteItems[i].className = "white";
-}	
+//Add colour to squares
 
-var blackItems = document.getElementsByTagName("li");
-for (var i = 1; i < 64; i += 2) {
-	blackItems[i].className = "black";
-}	
+var fillSquares = document.getElementsByTagName("li");
+	for (var i = 0; i < fillSquares.length; i+= 2){
+		fillSquares[i].className = "white";
+		if (i === 6 || i === 22 || i === 38 || i === 54) {
+			i++;
+	} 
+		if (i === 15 || i === 31 || i === 47) {
+			i--;
+	}
+}
+
+	for (var i = 1; i < fillSquares.length; i+= 2){
+		fillSquares[i].className = "black";
+		if (i === 7 || i === 23 || i === 39 || i === 55) {
+			i--;
+	} 
+		if (i === 14 || i === 30 || i === 46) {
+			i++;
+	}
+}
+
+
+//Add checkers
+// for (var i = 0; i < 8; i ++){
+// var lis = document.getElementsByTagName("li");
+// lis[1].innerHTML = ("&#x26c0");
+// }
+
+// var whiteItems = document.getElementsByTagName("li");
+// for (var i = 0; i < 64; i += 2) {
+// 	whiteItems[i].className = "white";
+// }	
+
+// var blackItems = document.getElementsByTagName("li");
+// for (var i = 1; i < 64; i += 2) {
+// 	blackItems[i].className = "black";
+// }	
