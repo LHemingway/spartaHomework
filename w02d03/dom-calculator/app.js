c80659e76553d5fe89e3d4e64471a7a20d35d831
Luke Hemingway <lukehemingway66@gmail.com>
2017-09-14 09:24:52 +0100

    Homework assignment w02d03 sums working

--- w02d03/dom-calculator/app.js
+++ w02d03/dom-calculator/app.js
@@ -11,18 +11,52 @@ document.addEventListener("DOMContentLoaded", function(){
 	var op1 = null;
 	var sum = null;
 
-//Do if val 1 already store as val 2 
+	//Funtions
+	function add(val1, val2){
+    return val1 + val2; 
+	}
+	function subtract(val1, val2){
+    return val1 - val2; 
+	}
+
+	function multiply(val1, val2){
+	return val1 * val2; 
+	}
+
+	function divide(val1, val2){
+	return val1 / val2; 
+	}
+
+	// function pow(val1, val2){
+	// sum = val1 ** val2; 
+	// }
+
+	// function sqroot(val1){
+	// sum = (Math.sqrt(val1));
+	// }
+
+	function clear(){
+		val1 = null;
+		val2 = null;
+		op1 = null;
+		// if(sum == null){
+		// 	display.innerText = "Giz sumat then..";
+		// }
+		sum = null;
+		display.innerText = null;
+	}
+
+
+
+
 	for(var i = 0; i < buttonNos.length; i++)	{
 		buttonNos[i].addEventListener("click", function() {
 			if (val1 == null){
 			display.innerText = this.value;
 			val1=parseInt(this.value);
-			console.log(val1);
 		} else {
-			//display.innerText = null;
 			display.innerText = this.value;
 			val2 = parseInt(this.value);
-			console.log(val2);
 		}
 		});
 	}
@@ -31,7 +65,6 @@ document.addEventListener("DOMContentLoaded", function(){
 		buttonOps[i].addEventListener("click", function() {
 			display.innerText = this.value;
 			op1=(this.value);
-			console.log(op1);
 		});
 	}
 
@@ -39,36 +72,21 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	buttonEqu.addEventListener("click", function(){
 		if (op1 ==="+") {
-			sum =  val1 + val2;
+			sum = add(val1, val2);
 		} else if (op1 === "-") {
-				sum = val1 - val2;
-			} else if (op1 === "*") {
-				sum = val1 * val2;
-				} else if (op1 === "/") {
-					sum = val1 / val2;
-					}
-		//sum=(this.value);
-		console.log(sum);
+			sum = subtract(val1, val2);
+		} else if (op1 === "*") {
+			sum = multiply(val1, val2);
+		} else if (op1 === "/") {
+			sum = divide(val1, val2);
+		}
+		
+		display.innerText = sum;
+		
 	}); 
 
 
-
-
-
 	buttonCe.addEventListener("click", function() {
-			val1 = null;
-			val2 = null;
-			op1 = null;
-			sum = null;
-		});
+		clear();
+	});
 });
-
-//Store values user inputs
-
-//parseInt Values into intergers from strings if true its number if not come back false
-
-//Change operators from strings to operators
-
-//Clear user inputs with c button
-
-//do the math
