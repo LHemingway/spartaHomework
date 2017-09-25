215d90217af3db73b2527d6956155cbc954073d5
Luke Hemingway <lukehemingway66@gmail.com>
2017-09-25 18:14:50 +0100

    calc and BMI working no methods

--- w04d01/calculator/calculator.rb
+++ w04d01/calculator/calculator.rb
@@ -14,8 +14,23 @@ def divide_numbers(num1, num2)
 	puts num1 / num2
 end
 
-puts "Pick a calculator type (b)asic or (a)dvanced"
+def power_numbers(num1, num2)
+	puts num1 ** num2
+end
+
+def square_root_numbers(num1)
+	puts Math.sqrt(num1)
+end
+
+def bmi_calc(weight, height)
+	sum = weight / height
+	puts sum / 1.75
+end
+
+
+puts "Pick a calculator type (b)asic or (a)dvanced or (bmi)"
 calc_type = gets.chomp
+
 
 if (calc_type === "b")
 	puts "basic baby"
@@ -23,11 +38,8 @@ if (calc_type === "b")
 	num1 = gets.chomp.to_f
 	puts "Enter the second number"
 	num2 = gets.chomp.to_f
-	puts "Enter your operand"
+	puts "Enter your operand (+, -, *, /)"
 	operand = gets.chomp
-elsif (calc_type === "a")
-
-	
 end
 
 if operand === "+"
@@ -40,6 +52,41 @@ elsif operand === "*"
 	multiply_numbers(num1, num2)
 else puts "You dun goofed"
 end
+
+if (calc_type === "a")
+	puts "advanced asshole"
+	puts "Enter the first number" 
+	num1 = gets.chomp.to_f
+	puts "Enter the second number"
+	num2 = gets.chomp.to_f
+	puts "Enter your operand (+, -, *, /) (Advanced only)(**, sq)"
+	operand = gets.chomp
+end
+
+if operand === "+"
+	add_numbers(num1, num2)
+elsif operand === "-"
+	subtract_numbers(num1, num2)
+elsif operand === "/"
+	divide_numbers(num1, num2)
+elsif operand === "*"
+	multiply_numbers(num1, num2)
+elsif operand === "**"
+	power_numbers(num1, num2)
+elsif operand === "sq"
+	square_root_numbers(num1)
+	else puts "You dun goofed"
+end
+
+if (calc_type === "bmi")
+	puts "BMI Bitch!"
+	puts "Enter the weight(kg)"
+	weight = gets.chomp.to_f
+	puts "Enter the height(M)" 
+	height = gets.chomp.to_f
+	bmi_calc(weight, height)
+end 
+
 
 # 	case operand
 # 		when operand === "+"
