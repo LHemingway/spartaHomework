ff1dfa3095f1145109b32dbbcb2e43227f6eb60b
Luke Hemingway <lukehemingway66@gmail.com>
2017-09-25 16:38:06 +0100

    start, writing some methods

---
+++ w04d01/calculator/calculator.rb
@@ -0,0 +1,21 @@
+def add_numbers(num1, num2)
+	puts num1 + num2
+end
+
+def subtract_numbers(num1, num2)
+	puts num1 - num2
+end
+
+def multiply_numbers(num1, num2)
+	puts num1 * num2
+end
+
+def divide_numbers(num1, num2)
+	puts num1 / num2
+end
+
+def selector(string)
+	puts "Pick a calculator dick breath (b)asic or (a)dvanced"
+	calc_type = gets.chomp
+	puts calc_type
+end
