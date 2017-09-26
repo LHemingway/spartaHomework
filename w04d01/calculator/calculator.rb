8107cede520a0db8c0d368d1a7ddcc1b4718f8ee
Luke Hemingway <lukehemingway66@gmail.com>
2017-09-26 09:39:58 +0100

    homework in final state imperial not working

--- w04d01/calculator/calculator.rb
+++ w04d01/calculator/calculator.rb
@@ -22,9 +22,16 @@ def square_root_numbers(num1)
 	puts Math.sqrt(num1)
 end
 
-def bmi_calc(weight, height)
+def bmi_calc_met(weight, height)
 	sum = weight / height
-	puts sum / 1.75
+	actual_bmi = sum / 1.75
+	puts actual_bmi
+end
+
+def bmi_calc_imp(weight_lb, height_inch)
+	sum = weight_lb / height_inch
+	actual_bmi = sum / 1.75
+	puts actual_bmi
 end
 
 
@@ -79,13 +86,24 @@ elsif operand === "sq"
 end
 
 if (calc_type === "bmi")
-	puts "BMI Bitch!"
-	puts "Enter the weight(kg)"
-	weight = gets.chomp.to_f
-	puts "Enter the height(M)" 
-	height = gets.chomp.to_f
-	bmi_calc(weight, height)
-end 
+	puts "BMI Bitches! You want (i)mperial or (m)etric"
+	measure_type = gets.chomp
+	if measure_type === "m"
+		puts "Enter the weight(kg)"
+		weight = gets.chomp.to_f
+		puts "Enter the height(M)" 
+		height = gets.chomp.to_f
+		pure_bmi = bmi_calc_met(weight, height)
+	end 
+end
+
+if measure_type === "i"
+	puts "Enter the weight(lb)"
+	weight_lb = gets.chomp.to_f
+	puts "Enter the height(inch)" 
+	height_inch = gets.chomp.to_f
+	pure_bmi = bmi_calc_imp(weight_lb, height_inch)
+end
 
 
 # 	case operand
